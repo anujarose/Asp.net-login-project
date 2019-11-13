1912e6500686f3f72a6b5f6d495ba1076e686cb3
anujarose <43371301+anujarose@users.noreply.github.com>
2019-11-13 12:46:24 +0530

    Add files via upload

---
+++ login/button.js
@@ -0,0 +1,13 @@
+ <!--
+            function Redirect() {
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+if ( username == "admin" && password == "admin123"){
+alert ("Login successfully");
+               window.location="2nd.html";
+}
+else{
+alert("enter the username and password");
+            }
+}
+         //-->
